07523b3109a2f4c25626441291fd14f27d028641
Monishprasad22 <monishprasad22@gmail.com>
2025-08-11 14:28:13 +0530

    Update monish.py

--- monish.py
+++ monish.py
@@ -1,7 +1,1 @@
-# Simple Python program to greet the user
-
-# Ask for the user's name
-name = input("What's your name? ")
-
-# Print a greeting
-print("Hello, " + name + "! Welcome to Python.")
+print("Prasad")
